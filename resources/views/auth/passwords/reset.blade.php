7b77b856b68bb5489da35f8bd739f1399a3ba2da
RobertusTriAjiNugroho672019123 <86818702+RobertusTriAjiNugroho672019123@users.noreply.github.com>
2022-10-03 17:31:58 +0700

    Membuat Halaman Front End Verifikasi Password AreaKerja.com

--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -1,26 +1,38 @@
 @extends('layouts.app')
 
+@section('css')
+    <link href="{{ asset('css/reset.css') }}" rel="stylesheet">
+@endsection
+
 @section('content')
     <div class="container">
         <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-
-                    <div class="card-header">{{ __('Reset Password') }}</div>
-
+            <div class="col d-none d-lg-flex pt-4">
+                <img class="w-82" src="{{ asset('img/reset.svg') }}" alt="" srcset="">
+            </div>
+            <div class="col-md-5">
+                <div class="card border-0 bg-transparent">
+                    <div class="row m-0">
+                        <h2 class="judul card-header bg-transparent border-0 m-0">{{ __('Reset Password') }}</h2>
+                        <div class="row">
+                            <p>Kami telah mengirimkan kode verifikasi ke email Anda <span
+                                    class="kata">cust.areakerja@gmail.com</span>
+                            </p>
+                        </div>
+                    </div>
                     <div class="card-body">
                         <form method="POST" action="{{ route('password.update') }}">
                             @csrf
 
-                            <input type="hidden" name="token" value="{{ $token }}">
+                            <input type="hidden" name="token" cl value="{{ $token }}">
 
                             <div class="row mb-3">
                                 <label for="email"
-                                    class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
+                                    class="col-md-4 col-form-label text-md-start">{{ __('Email') }}</label>
 
                                 <div class="col-md-6">
                                     <input id="email" type="email"
-                                        class="form-control @error('email') is-invalid @enderror" name="email"
+                                        class="form-control reset2 @error('email') is-invalid @enderror" name="email"
                                         value="{{ $email ?? old('email') }}" required autocomplete="email" autofocus>
 
                                     @error('email')
@@ -33,11 +45,11 @@
 
                             <div class="row mb-3">
                                 <label for="password"
-                                    class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
+                                    class="col-md-4 col-form-label text-md-starts">{{ __('Password') }}</label>
 
                                 <div class="col-md-6">
                                     <input id="password" type="password"
-                                        class="form-control @error('password') is-invalid @enderror" name="password"
+                                        class="form-control reset2 @error('password') is-invalid @enderror" name="password"
                                         required autocomplete="new-password">
 
                                     @error('password')
@@ -50,17 +62,17 @@
 
                             <div class="row mb-3">
                                 <label for="password-confirm"
-                                    class="col-md-4 col-form-label text-md-end">{{ __('Confirm Password') }}</label>
+                                    class="col-md-4 col-form-label text-md-start">{{ __('Passowd Conform ') }}</label>
 
                                 <div class="col-md-6">
-                                    <input id="password-confirm" type="password" class="form-control"
+                                    <input id="password-confirm" type="password" class="form-control reset2"
                                         name="password_confirmation" required autocomplete="new-password">
                                 </div>
                             </div>
-
+                            <p class="text-center ">Kirim Kode Ulang</p>
                             <div class="row mb-0">
                                 <div class="col-md-6 offset-md-4">
-                                    <button type="submit" class="btn btn-primary">
+                                    <button type="submit" class="btn btn-primary reset1">
                                         {{ __('Reset Password') }}
                                     </button>
                                 </div>
