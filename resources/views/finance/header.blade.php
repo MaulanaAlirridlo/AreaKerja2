e8e6c4be989829ec7c2d953cdb83d392d839f9b4
MaulanaAlirridlo <maulana.alirridlo@gmail.com>
2022-10-04 19:42:22 +0700

    Menambahkan activity di halaman finance

--- resources/views/finance/header.blade.php
+++ resources/views/finance/header.blade.php
@@ -25,6 +25,7 @@
 
 
     <link rel="stylesheet" href="{{asset('css/style.css')}}">
+    <link rel="stylesheet" href="{{asset('css/activity.css')}}">
 
     <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,600,700,800' rel='stylesheet' type='text/css'>
 
